4caad86f772f4608ab442902a34642b2390a1a5d
Vijayakumar <vijayakumardpr@gmail.com>
2022-05-17 16:03:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import data from "./data"
+import Navbar from "./components/Navbar"
+import Travels from "./components/Travels"
+import "./styles.css"
+
+export default function App() {
+  const cards = data.map((item) => {
+    return <Travels key={item.title} {...item} />
+  })
+  return (
+    <div>
+      <Navbar />
+      {cards}
+    </div>
+  )
+}
